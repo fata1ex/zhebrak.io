d3a06e47e735d267d652e41ce4eaea2d9b8e0991
Alexander Zhebrak <fata2ex@gmail.com>
2016-02-28 13:55:54 -0500

    dollar url

--- app/urls.py
+++ app/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
 
     url(r'', TemplateView.as_view(template_name="index.html")),
 
-    url(r'^composer/$', include('composer.urls'))
+    url(r'^composer/', include('composer.urls'))
 ]
