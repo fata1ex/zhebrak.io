55fa729713818411085b180465085a45bd374932
Alexander Zhebrak <fata2ex@gmail.com>
2016-02-28 14:04:35 -0500

    switch urls

--- app/urls.py
+++ app/urls.py
@@ -9,7 +9,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'', TemplateView.as_view(template_name="index.html")),
+    url(r'^composer/', include('composer.urls')),
 
-    url(r'^composer/', include('composer.urls'))
+    url(r'', TemplateView.as_view(template_name="index.html")),
 ]
