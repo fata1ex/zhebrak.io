7fb4f86bf80d8b37230467a04eb78eabea46b89c
Alexander Zhebrak <fata2ex@gmail.com>
2016-09-07 18:55:50 +0300

    homepage

--- app/urls.py
+++ app/urls.py
@@ -5,14 +5,18 @@ from django.contrib import admin
 from django.shortcuts import render_to_response
 from django.views.generic import TemplateView
 
+import app.views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^composer/', include('composer.urls')),
 
+    url(r'^loader/', apps.views.loader),
     url(r'', TemplateView.as_view(template_name="index.html")),
 ]
+
 
 handler400 = handler403 = handler404 = handler500 = 'app.urls.error_index'
 
